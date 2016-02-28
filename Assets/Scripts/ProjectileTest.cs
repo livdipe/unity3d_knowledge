ba684ea8f24826fa68a5d596eb9738b778d8196e
lcl <livdipe@gmail.com>
2016-02-28 21:17:30 +0800

    30度抛物炮弹击中目标实验

---
+++ Assets/Scripts/ProjectileTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileTest : MonoBehaviour 
+{
+	public GameObject target;
+	public float speed = 10;
+	private float distanceToTarget;
+	private bool move = true;
+
+	//新建两个Sphere，其中一个挂该脚本，另一个做为target,拖拽到以上target变量上
+
+	void Start()
+	{
+		distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+	}
+
+	void Update()
+	{
+		if (move)
+		{
+			transform.LookAt(target.transform.position);
+			float currentDist = Vector3.Distance(transform.position, target.transform.position);
+			float angle = Mathf.Min(1, currentDist / distanceToTarget) * 30;
+			transform.rotation = transform.rotation * Quaternion.Euler(Mathf.Clamp(-angle, -30, 30), 0, 0);
+			if (currentDist < 0.1f)
+				move = false;
+			transform.Translate(Vector3.forward * Mathf.Min(speed * Time.deltaTime, currentDist));
+		}
+	}
+}
