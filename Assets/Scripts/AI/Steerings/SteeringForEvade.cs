2e1da1bdc7c0a977a2cbfcdfa1d6b6691f6c8907
lcl <livdipe@gmail.com>
2016-06-17 08:17:58 +0800

    逃避，徘徊，电子书

---
+++ Assets/Scripts/AI/Steerings/SteeringForEvade.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringForEvade : Steering 
+{
+	public GameObject target;
+
+	private Vector2 desireVelocity;
+
+	private Vehicle m_vehicle;
+
+	void Start()
+	{
+		m_vehicle = GetComponent<Vehicle>();
+	}
+
+	public override Vector2 Force()
+	{
+		Vector2 toTarget = target.transform.position - transform.position;
+		Vehicle targetVehicle = target.GetComponent<Vehicle>();
+		float lookaheadTime = toTarget.magnitude / (m_vehicle.maxSpeed + targetVehicle.velocity.magnitude);
+		toTarget = transform.position - target.transform.position;
+		toTarget += targetVehicle.velocity * lookaheadTime;
+		desireVelocity = toTarget.normalized * m_vehicle.maxSpeed;
+
+		return (desireVelocity - m_vehicle.velocity);
+	}
+}
---
+++ Assets/Scripts/AI/Steerings/SteeringForWander.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringForWander : Steering 
+{
+	//徘徊半径，即Wander圈的半径
+	public float wanderRadius = 2;
+	//徘徊距离, 即wander圈凸出在AI角色前面的距离
+	public float wanderDistance;
+	//每秒加到目标的随机位移的最大值
+	public float wanderJitter;
+
+	private Vector2 desireVelocity;
+
+	private Vehicle m_vehicle;
+
+	private Vector2 circleTarget;
+	private Vector2 wanderTarget;
+
+	void Start()
+	{
+		m_vehicle = GetComponent<Vehicle>();
+		circleTarget = new Vector2(wanderRadius * 0.707f, wanderRadius * 0.707f);
+	}
+
+	public override Vector2 Force()
+	{
+		Vector2 randomDisplacement = new Vector2((Random.value - 0.5f) * 2 * wanderJitter,
+			(Random.value - 0.5f) * 2 * wanderJitter);
+
+		circleTarget += randomDisplacement;
+
+		circleTarget = wanderRadius * circleTarget.normalized;
+
+		wanderTarget = m_vehicle.velocity.normalized * wanderDistance + circleTarget;
+
+		desireVelocity = wanderTarget.normalized * m_vehicle.maxSpeed;
+
+		return (desireVelocity - m_vehicle.velocity);
+
+	}
+}
