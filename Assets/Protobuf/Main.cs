e8894ef83280eb5868b5fe8ac958f939490a29b1
lcl <livdipe@gmail.com>
2016-07-08 18:12:12 +0800

    protobuf的使用

---
+++ Assets/Protobuf/Main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using test;
+
+namespace ProtobufDemo
+{
+	public class Main : MonoBehaviour 
+	{
+		void Start () 
+		{
+			Person person = new Person();
+			person.age = 28;
+			person.name = "lcl";
+
+			byte[] bytes = Serialization.Serialize<Person>(person);
+			Debug.Log("序列化bytes length:" + bytes.Length);
+			Person person2 = Serialization.Deserialize<Person>(bytes);
+			Debug.Log("反列化 age:" + person2.age);
+		}
+	}
+}
---
+++ Assets/Protobuf/Serialization.cs
@@ -0,0 +1,28 @@
+﻿using ProtoBuf;
+using System;
+using System.IO;
+
+public static class Serialization 
+{
+	public static byte[] Serialize<T>(T instance)
+	{
+		byte[] bytes;
+		using (var ms = new MemoryStream())
+		{
+			Serializer.Serialize(ms, instance);
+			bytes = new byte[ms.Position];
+			var fullBytes = ms.GetBuffer();
+			Array.Copy(fullBytes, bytes, bytes.Length);
+		}
+		return bytes;
+	}
+
+	public static T Deserialize<T>(object obj)
+	{
+		byte[] bytes = (byte[])obj;
+		using (var ms = new MemoryStream(bytes))
+		{
+			return Serializer.Deserialize<T>(ms);
+		}
+	}
+}
