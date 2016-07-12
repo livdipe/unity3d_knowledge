819f5dae857d1a8c0b9275a3e22d1d71716983c4
lcl <livdipe@gmail.com>
2016-07-12 23:37:10 +0800

    桥接模式，不同手机跟不同手机应用之间的关系

---
+++ Assets/DesignPattern/BridgePattern/BridgePatternDemo.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 手机上可以安装音乐、游戏等app，也可以不安装，不管安不安装这些app
+// 都不会影响手机的使用，由此可见手机和app之间的关系是一种聚合关系
+// 把手机和app分离开
+// 尽量使用组合/聚合，而不要使用类继承
+namespace BridgePatternDemo
+{
+	public abstract class MobilePhoneApp
+	{
+		public abstract void Run();
+	}
+
+	public class Music : MobilePhoneApp
+	{
+		public override void Run()
+		{
+			Debug.Log("运行手机音乐");
+		}
+	}
+
+	public class Game : MobilePhoneApp
+	{
+		public override void Run()
+		{
+			Debug.Log("运行手机游戏");
+		}
+	}
+
+	public abstract class MobilePhone
+	{
+		protected MobilePhoneApp app;
+
+		public MobilePhone(MobilePhoneApp app)
+		{
+			this.app = app;
+		}
+
+		public abstract void Run();
+	}
+
+	public class MEIZU : MobilePhone
+	{
+		public MEIZU(MobilePhoneApp app) : base(app)
+		{
+		}
+
+		public override void Run()
+		{
+			this.app.Run();
+		}
+	}
+
+	public class IPhone : MobilePhone
+	{
+		public IPhone(MobilePhoneApp app) : base(app)
+		{
+		}
+
+		public override void Run()
+		{
+			this.app.Run();
+		}
+	}
+}
