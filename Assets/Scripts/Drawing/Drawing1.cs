1d4eed78e63ab6b5c4a1e953df5e3c09e8fe42ac
lcl <livdipe@gmail.com>
2016-07-12 23:39:27 +0800

    使用LineRenderer组件画线，连续的线

---
+++ Assets/Scripts/Drawing/Drawing1.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Drawing1 : MonoBehaviour 
+{
+	private LineRenderer line;
+	private Material material;
+	private bool isMousePressed;
+	public List<Vector3> pointsList;
+	private Vector3 mousePos;
+
+	void Start () 
+	{
+		material = new Material(Shader.Find("Particles/Additive"));
+		line = gameObject.AddComponent<LineRenderer>();
+		line.material = material;
+		line.SetVertexCount(1);
+		line.SetWidth(0.1f, 0.1f);
+		line.SetColors(Color.green, Color.green);
+		line.useWorldSpace = true;
+		isMousePressed = false;
+		pointsList = new List<Vector3>();
+	}
+	
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			isMousePressed = true;
+			line.SetVertexCount(0);
+			pointsList.RemoveRange(0, pointsList.Count);
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			isMousePressed = false;
+		}
+
+		if (isMousePressed)
+		{
+			mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			mousePos.z = 0;
+			if (!pointsList.Contains(mousePos))
+			{
+				pointsList.Add(mousePos);
+				line.SetVertexCount(pointsList.Count);
+				line.SetPosition(pointsList.Count - 1, pointsList[pointsList.Count-1]);
+			}
+		}
+	}
+}
