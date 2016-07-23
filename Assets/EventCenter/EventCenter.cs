1339ff21ed7e54e1c3b73fa2d335dc4571199e0a
lcl <livdipe@gmail.com>
2016-07-23 19:06:02 +0800

    EventCenter

---
+++ Assets/EventCenter/EventCenter.cs
@@ -0,0 +1,96 @@
+﻿namespace EventCenterDemo
+{
+	using System;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	static internal class EventCenter 
+	{
+		public delegate void Callback();
+		public delegate void Callback<T>(T arg1);
+
+		static public Dictionary<string, Delegate> mEventTable = new Dictionary<string, Delegate>();
+
+		static public void OnListenerAdding(string eventType, Delegate listenerBeingAdded)
+		{
+			if (!mEventTable.ContainsKey(eventType))
+			{
+				mEventTable.Add(eventType, null);
+			}
+
+			Delegate d = mEventTable[eventType];
+			if (d != null && d.GetType() != listenerBeingAdded.GetType())
+			{
+				Debug.LogError("委托方法类型不同!");
+			}
+		}
+
+		static public void OnListenerRemoved(string eventType)
+		{
+			if (mEventTable[eventType] == null)
+			{
+				mEventTable.Remove(eventType);
+			}
+		}
+
+		// 没有参数
+		static public void AddListener(string eventType, Callback handler)
+		{
+			OnListenerAdding(eventType, handler);
+			mEventTable[eventType] = (Callback)mEventTable[eventType] + handler;
+		}
+
+		// Single parameter
+		static public void AddListener<T>(string eventType, Callback<T> handler)
+		{
+			OnListenerAdding(eventType, handler);
+			mEventTable[eventType] = (Callback<T>)mEventTable[eventType] + handler;
+		}
+
+		static public void RemoveListener(string eventType, Callback handler)
+		{
+			mEventTable[eventType] = (Callback)mEventTable[eventType] - handler;
+			OnListenerRemoved(eventType);
+		}
+
+		static public void RemoveListener<T>(string eventType, Callback<T> handler)
+		{
+			mEventTable[eventType] = (Callback<T>)mEventTable[eventType] - handler;
+			OnListenerRemoved(eventType);
+		}
+
+		static public void Broadcast(string eventType)
+		{
+			Delegate d;
+			if (mEventTable.TryGetValue(eventType, out d))
+			{
+				Callback callback = d as Callback;
+
+				if (callback != null)
+				{
+					callback();
+				}
+			}
+		}
+
+		static public void Broadcast<T>(string eventType, T arg1)
+		{
+			Delegate d;
+			if (mEventTable.TryGetValue(eventType, out d))
+			{
+				Callback<T> callback = d as Callback<T>;
+
+				if (callback != null)
+				{
+					callback(arg1);
+				}
+			}
+		}
+
+		static public void Cleanup()
+		{
+			mEventTable.Clear();
+		}
+	}
+
+}
---
+++ Assets/EventCenter/Main.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EventCenterDemo
+{
+	public class Main : MonoBehaviour 
+	{
+		void Start () 
+		{
+			EventCenter.AddListener("MouseHit", MouseHit);
+			EventCenter.AddListener<int>("MouseHit1", MouseHit1);
+		}
+
+		void MouseHit()
+		{
+			Debug.Log("MouseHit");
+		}
+
+		void MouseHit1(int arg1)
+		{
+			Debug.Log("MouseHit1");
+		}
+
+		void Update()
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				EventCenter.Broadcast("MouseHit");
+				EventCenter.Broadcast<int>("MouseHit1", 563);
+			}
+		}
+	}
+}
