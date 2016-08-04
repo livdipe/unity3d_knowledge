0e8899d763a3f2eb482bdf222b49ce66f8659e59
lcl <livdipe@gmail.com>
2016-08-05 07:00:23 +0800

    准备用Unity实现《游戏人工智能编程案例精粹》中的操控行为

---
+++ Assets/GameAIByExample/SteeringBehaviors.cs
@@ -0,0 +1,28 @@
+﻿namespace GameAI
+{
+	using UnityEngine;
+	using System.Collections;
+
+	public class SteeringBehaviors 
+	{
+		private Vehicle m_pVehicle;
+		public Vector2 m_vTarget;
+
+		public SteeringBehaviors(Vehicle vehicle)
+		{
+			m_pVehicle = vehicle;
+		}
+
+		public Vector2 Calculate()
+		{
+			return Seek(m_vTarget);
+		}
+
+		public Vector2 Seek(Vector2 TargetPos)
+		{
+			Vector2 DesiredVelocity = (TargetPos - m_pVehicle.m_vPos).normalized * m_pVehicle.MaxSpeed();
+
+			return DesiredVelocity - m_pVehicle.Velocity();
+		}
+	}
+}
---
+++ Assets/GameAIByExample/Vehicle.cs
@@ -0,0 +1,76 @@
+﻿namespace GameAI
+{
+	using UnityEngine;
+	using System.Collections;
+
+	public class Vehicle : MonoBehaviour 
+	{
+		public Vector2 m_vPos;
+		Vector2 m_vVelocity;
+		public Vector2 m_vHeading;
+		public Vector2 m_vSide;
+		public float m_dMass;
+		public float m_dMaxSpeed;
+		public float m_dMaxForce;
+		public float m_dMaxTurnRate;
+		SteeringBehaviors m_pSteering;
+
+		public Transform trTarget;
+
+		void Start () 
+		{
+			m_pSteering = new SteeringBehaviors(this);
+			InvokeRepeating("ChangeTarget", 2, 20);
+		}
+
+		void ChangeTarget()
+		{
+			m_pSteering.m_vTarget = new Vector2(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+			trTarget.position = m_pSteering.m_vTarget;
+		}
+		
+		void Update () 
+		{
+			Vector2 SteeringForce = m_pSteering.Calculate();
+
+			//加速度 ＝ 力 / 质量
+			Vector2 acceleration = SteeringForce / m_dMass;
+
+			//更新速度
+			m_vVelocity += acceleration * Time.deltaTime;
+
+			if (m_vVelocity.magnitude > m_dMaxSpeed)
+			{
+				m_vVelocity = m_vVelocity.normalized * m_dMaxSpeed;
+			}
+
+			//更新位置
+			m_vPos += m_vVelocity * Time.deltaTime;
+
+			if (m_vVelocity.sqrMagnitude > 0.0000001f)
+			{
+				m_vHeading = m_vVelocity.normalized;
+
+				m_vSide = Perp(m_vHeading); 
+			}
+
+			transform.position = m_vPos;
+
+		}
+
+		Vector2 Perp(Vector2 v)
+		{
+			return new Vector2(-v.y, v.x);
+		}
+
+		public float MaxSpeed()
+		{
+			return m_dMaxSpeed;
+		}
+
+		public Vector2 Velocity()
+		{
+			return m_vVelocity;
+		}
+	}
+}
