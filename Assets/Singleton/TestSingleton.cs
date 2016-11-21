5c75927859ca87f59b88f67af1a55745868aa29e
lcl <livdipe@gmail.com>
2016-11-21 14:58:02 +0800

    一个通用Unity单例实现

---
+++ Assets/Singleton/TestSingleton.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSingleton : GenericSingleton<TestSingleton>
+{
+	public int number = 123456;
+}
---
+++ Assets/Singleton/TestSingleton2.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSingleton2 : MonoBehaviour 
+{
+	void Start () 
+	{
+		Debug.LogError(TestSingleton.Instance.number);
+	}
+}
